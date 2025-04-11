535de68f88b862e3fc7b615efd8cca6a676e89a6
danialedilev <danial.200316@gmail.com>
2025-04-12 01:29:03 +0500

    feat: remove cors registry

--- src/main/kotlin/kz/team/pizzaapp/config/WebConfig.kt
+++ src/main/kotlin/kz/team/pizzaapp/config/WebConfig.kt
@@ -7,6 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**").allowedOrigins("*") // 🚫 remove this if present
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*") // 🚫 remove this if present
     }
 }
